a5575478ea213c43b8b79d0ee45525bea094f2f7
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2024-10-18 12:53:19 -0400

    wip: conway's game of life

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,17 @@
 #![no_main]
 
+// TODO: rename crate
+use fibonacci::Universe;
+
 entrypoint::entrypoint!(main);
 
 pub fn main() {
-    // NOTE: values of n larger than 46 will overflow the u32 type.
-    let n = unsafe { entrypoint::io::getchar() as u8 };
-    let mut a: u32 = 0;
-    let mut b: u32 = 1;
-    let mut sum: u32;
-    for _ in 1..n {
-            sum = a + b;
-            a = b;
-            b = sum;
-        }
+    let mut universe = Universe::new();
+    for i in 0..100 {
+        universe.tick();
+    }
 
-    entrypoint::io::println(&n.to_string());
-    entrypoint::io::println("-th fibonacci number is:");
-    entrypoint::io::println(&b.to_string());
+//     entrypoint::io::println(&n.to_string());
+//     entrypoint::io::println("-th fibonacci number is:");
+//     entrypoint::io::println(&b.to_string());
 }
