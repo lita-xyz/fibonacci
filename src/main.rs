d64497cea86967c92271712f5a8510b66f593b78
Lewis Revill <lewis1revill@gmail.com>
2024-10-22 16:18:53 +0100

    Print cell state after 10 iterations
    This changes two things, first we were only printing the calculated
    index, and secondly, the initial state disappears by the time we reach
    100 iterations, so we should print just after 10 to actually have
    something to show.

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,14 @@ entrypoint::entrypoint!(main);
 
 pub fn main() {
     let mut universe = Universe::new();
-    for i in 0..100 {
+    for i in 0..10 {
         universe.tick();
     }
     for i in 0..10 {
         for j in 0..10 {
-            entrypoint::io::println(universe.get_index(i, j).to_string().as_str())
+            let idx = universe.get_index(i, j);
+            let cell = universe.get_cells()[idx] as u8;
+            entrypoint::io::println(cell.to_string().as_str())
         }
     }
 }
